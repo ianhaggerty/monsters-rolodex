4aec705f526da8ab580fbe9592d999dc2fd3707a
Ian Haggerty <oscar@Ians-iMac.local>
2019-07-10 07:35:23 +0100

    Updates

---
+++ precache-manifest.3bea186d0ecfe5b6d56449dd7c266796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9549b4977853b38170c06026d89b21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "93a054902364211ea396",
+    "url": "/monsters-rolodex/static/css/main.f3a765dc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "93a054902364211ea396",
+    "url": "/monsters-rolodex/static/js/main.20e58cae.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
